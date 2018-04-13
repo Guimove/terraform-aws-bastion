927d82d0ebd4110640328bdaae37cc7ec11b6ca7
Guimove <dasilva.guillaume@live.fr>
2018-04-13 10:12:39 +0200

    correct typo mistake on scaling

--- main.tf
+++ main.tf
@@ -1,48 +1,56 @@
 data "template_file" "user_data" {
   template = "${file("${path.module}/user_data.sh")}"
+
   vars {
-    aws_region = "${var.region}"
+    aws_region  = "${var.region}"
     bucket_name = "${var.bucket_name}"
   }
 }
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}"
-  acl = "bucket-owner-full-control"
+  acl    = "bucket-owner-full-control"
 
   versioning {
     enabled = true
   }
+
   tags = "${merge(var.tags)}"
 }
 
 resource "aws_security_group" "bastion_host_security_group" {
   description = "Enable SSH access to the bastion host from external via port 22"
-  vpc_id = "${var.vpc_id}"
+  vpc_id      = "${var.vpc_id}"
+
   ingress {
-    from_port = 22
-    protocol = "TCP"
-    to_port = 22
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
     cidr_blocks = "${var.cidrs}"
   }
+
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = "${merge(var.tags)}"
 }
 
 resource "aws_security_group" "private_instances_security_group" {
   description = "Enable SSH access to the Private instances from the bastion via port 22"
-  vpc_id = "${var.vpc_id}"
+  vpc_id      = "${var.vpc_id}"
+
   ingress {
     from_port = 22
-    protocol = "TCP"
-    to_port = 22
+    protocol  = "TCP"
+    to_port   = 22
+
     security_groups = [
-      "${aws_security_group.bastion_host_security_group.id}"]
+      "${aws_security_group.bastion_host_security_group.id}",
+    ]
   }
 
   tags = "${merge(var.tags)}"
@@ -50,6 +58,7 @@ resource "aws_security_group" "private_instances_security_group" {
 
 resource "aws_iam_role" "bastion_host_role" {
   path = "/"
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -72,6 +81,7 @@ EOF
 
 resource "aws_iam_role_policy" "bastion_host_role_policy" {
   role = "${aws_iam_role.bastion_host_role.id}"
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -105,45 +115,52 @@ EOF
 }
 
 resource "aws_route53_record" "bastion_record_name" {
-  name = "${var.bastion_record_name}"
-  type = "CNAME"
+  name    = "${var.bastion_record_name}"
+  type    = "CNAME"
   zone_id = "${var.hosted_zone_name}"
-  ttl = 300
+  ttl     = 300
+
   records = [
-    "${aws_lb.bastion_lb.dns_name}"
+    "${aws_lb.bastion_lb.dns_name}",
   ]
 }
 
 resource "aws_lb" "bastion_lb" {
   internal = "${var.is_lb_private}"
+
   subnets = [
-    "${var.elb_subnets}"]
+    "${var.elb_subnets}",
+  ]
+
   load_balancer_type = "network"
-  tags = "${merge(var.tags)}"
+  tags               = "${merge(var.tags)}"
 }
 
 resource "aws_lb_target_group" "bastion_lb_target_group" {
-  port = 22
-  protocol = "TCP"
-  vpc_id = "${var.vpc_id}"
+  port        = 22
+  protocol    = "TCP"
+  vpc_id      = "${var.vpc_id}"
   target_type = "instance"
+
   health_check {
-    port = "traffic-port"
-    protocol = "TCP"
-    healthy_threshold = "${var.bastion_instance_count}"
+    port                = "traffic-port"
+    protocol            = "TCP"
+    healthy_threshold   = "${var.bastion_instance_count}"
     unhealthy_threshold = "${var.bastion_instance_count}"
   }
+
   tags = "${merge(var.tags)}"
 }
 
 resource "aws_lb_listener" "bastion_lb_listener_22" {
   "default_action" {
     target_group_arn = "${aws_lb_target_group.bastion_lb_target_group.arn}"
-    type = "forward"
+    type             = "forward"
   }
+
   load_balancer_arn = "${aws_lb.bastion_lb.arn}"
-  port = 22
-  protocol = "TCP"
+  port              = 22
+  protocol          = "TCP"
 }
 
 resource "aws_iam_instance_profile" "bastion_host_profile" {
@@ -152,16 +169,19 @@ resource "aws_iam_instance_profile" "bastion_host_profile" {
 }
 
 resource "aws_launch_configuration" "bastion_launch_configuration" {
-  image_id = "${lookup(var.bastion_amis, var.region)}"
-  instance_type = "t2.nano"
+  image_id                    = "${lookup(var.bastion_amis, var.region)}"
+  instance_type               = "t2.nano"
   associate_public_ip_address = true
-  enable_monitoring = true
-  iam_instance_profile = "${aws_iam_instance_profile.bastion_host_profile.name}"
-  key_name = "${var.bastion_host_key_pair}"
+  enable_monitoring           = true
+  iam_instance_profile        = "${aws_iam_instance_profile.bastion_host_profile.name}"
+  key_name                    = "${var.bastion_host_key_pair}"
+
   security_groups = [
-    "${aws_security_group.bastion_host_security_group.id}"
+    "${aws_security_group.bastion_host_security_group.id}",
   ]
+
   user_data = "${data.template_file.user_data.rendered}"
+
   lifecycle {
     create_before_destroy = true
   }
@@ -169,18 +189,22 @@ resource "aws_launch_configuration" "bastion_launch_configuration" {
 
 resource "aws_autoscaling_group" "bastion_auto_scaling_group" {
   launch_configuration = "${aws_launch_configuration.bastion_launch_configuration.name}"
-  max_size = "${var.bastion_instance_count}"
-  min_size = "${var.bastion_instance_count}"
-  desired_capacity = "${var.bastion_instance_count}"
+  max_size             = "${var.bastion_instance_count}"
+  min_size             = "${var.bastion_instance_count}"
+  desired_capacity     = "${var.bastion_instance_count}"
+
   vpc_zone_identifier = [
-    "${var.auto_scalling_group_subnets}"
+    "${var.auto_scaling_group_subnets}",
   ]
-  default_cooldown = 180
+
+  default_cooldown          = 180
   health_check_grace_period = 180
-  health_check_type = "EC2"
+  health_check_type         = "EC2"
+
   target_group_arns = [
-    "${aws_lb_target_group.bastion_lb_target_group.arn}"
+    "${aws_lb_target_group.bastion_lb_target_group.arn}",
   ]
+
   lifecycle {
     create_before_destroy = true
   }
--- variables.tf
+++ variables.tf
@@ -4,17 +4,18 @@ variable "bucket_name" {
 
 variable "tags" {
   description = "A mapping of tags to assign"
-  default = {}
-  type = "map"
+  default     = {}
+  type        = "map"
 }
 
 variable "region" {}
 
 variable "cidrs" {
   description = "List of CIDRs than can access to the bastion. Default : 0.0.0.0/0"
-  type = "list"
+  type        = "list"
+
   default = [
-    "0.0.0.0/0"
+    "0.0.0.0/0",
   ]
 }
 
@@ -39,28 +40,29 @@ variable "bastion_record_name" {
 }
 
 variable "elb_subnets" {
-  type = "list"
+  type        = "list"
   description = "List of subnet were the ELB will be deployed"
 }
 
-variable "auto_scalling_group_subnets" {
-  type = "list"
+variable "auto_scaling_group_subnets" {
+  type        = "list"
   description = "List of subnet were the Auto Scalling Group will deploy the instances"
 }
 
 variable "bastion_amis" {
   type = "map"
+
   default = {
-    "us-east-1" = "ami-f5f41398"
-    "us-west-2" = "ami-d0f506b0"
-    "us-west-1" = "ami-6e84fa0e"
-    "eu-west-1" = "ami-b0ac25c3"
-    "eu-central-1" = "ami-d3c022bc"
+    "us-east-1"      = "ami-f5f41398"
+    "us-west-2"      = "ami-d0f506b0"
+    "us-west-1"      = "ami-6e84fa0e"
+    "eu-west-1"      = "ami-b0ac25c3"
+    "eu-central-1"   = "ami-d3c022bc"
     "ap-southeast-1" = "ami-1ddc0b7e"
     "ap-northeast-2" = "ami-cf32faa1"
     "ap-northeast-1" = "ami-29160d47"
     "ap-southeast-2" = "ami-0c95b86f"
-    "sa-east-1" = "ami-fb890097"
+    "sa-east-1"      = "ami-fb890097"
   }
 }
 

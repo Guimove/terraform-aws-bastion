1c7af4527c45bb7e7152b9ff128d9ff6f4db2ee0
Guimove <dasilva.guillaume@live.fr>
2018-04-19 17:31:27 +0200

    Fix #11 allow egress http

--- main.tf
+++ main.tf
@@ -62,6 +62,13 @@ resource "aws_security_group" "bastion_host_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = "${merge(var.tags)}"
 }
 

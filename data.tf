2aaef2890d32d6266e11cae818c8c88bb6119d3d
guimove <dasilva.guillaume@live.fr>
2019-03-04 15:46:20 +0100

    add ami owner

--- data.tf
+++ data.tf
@@ -1,16 +1,12 @@
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
-  
+
   filter {
     name   = "architecture"
     values = ["x86_64"]

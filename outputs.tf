775d3ed179a1b7acce72c3c2664872b30303c05a
Paul Lucas <paul.lucas@infinityworks.com>
2019-04-02 16:08:16 +0100

    Fix for issue #2

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "elb_ip" {
   value = "${aws_lb.bastion_lb.dns_name}"
 }
 
+output "bastion_host_security_group" {
+  value = "${aws_security_group.bastion_host_security_group.id}"
+}
+
 output "private_instances_security_group" {
   value = "${aws_security_group.private_instances_security_group.id}"
 }

9d40966219520051ea50b8e55335dfcd1706dbe8
Tomasz Szuster <>
2019-12-04 01:44:01 +0100

    :memo: Added more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,21 @@
+output "bastion_host_security_group" {
+  value = aws_security_group.bastion_host_security_group.id
+}
+
+output "bucket_kms_key_alias" {
+  value = aws_kms_alias.alias.name
+}
+
+output "bucket_kms_key_arn" {
+  value = aws_kms_key.key.arn
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.bucket.bucket
 }
 
 output "elb_ip" {
   value = aws_lb.bastion_lb.dns_name
-}
-
-output "bastion_host_security_group" {
-  value = aws_security_group.bastion_host_security_group.id
 }
 
 output "private_instances_security_group" {

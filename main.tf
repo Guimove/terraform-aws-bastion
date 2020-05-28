6a0cb00795d7cdf7c1bf2fff92bbb93cef57af34
Anton Winter <anton@switchbit.com>
2020-05-27 17:34:43 -0700

    fix: make unique iam policy name

--- main.tf
+++ main.tf
@@ -184,7 +184,7 @@ data "aws_iam_policy_document" "bastion_host_policy_document" {
 }
 
 resource "aws_iam_policy" "bastion_host_policy" {
-  name   = "BastionHost-${local.name_prefix}"
+  name_prefix   = "BastionHost"
   policy = data.aws_iam_policy_document.bastion_host_policy_document.json
 }
 

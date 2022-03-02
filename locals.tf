e6332cdd7daf1b7fa004e248b58b07c7727660f7
Guillaume <dasilva.guillaume@live.fr>
2022-03-02 18:36:18 +0100

    Add support for AWS v4 (#131)
    * Add support for AWS v4

--- locals.tf
+++ locals.tf
@@ -1,17 +1,5 @@
 locals {
-  tags_asg_format = null_resource.tags_as_list_of_maps.*.triggers
-
   name_prefix    = var.bastion_launch_template_name
   security_group = join("", flatten([aws_security_group.bastion_host_security_group[*].id, var.bastion_security_group_id]))
-}
-
-resource "null_resource" "tags_as_list_of_maps" {
-  count = length(keys(var.tags))
-
-  triggers = {
-    "key"                 = element(keys(var.tags), count.index)
-    "value"               = element(values(var.tags), count.index)
-    "propagate_at_launch" = "true"
-  }
 }
 

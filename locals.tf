95e2ceb6fb8e629ac9eb38db3d66c423e9da27f4
alsidneio <alsidneio@gmail.com>
2022-03-07 23:41:59 -0800

    updating main.tf and adding to locals.tf to resolve ipv6 error\

--- locals.tf
+++ locals.tf
@@ -1,5 +1,10 @@
 locals {
   name_prefix    = var.bastion_launch_template_name
   security_group = join("", flatten([aws_security_group.bastion_host_security_group[*].id, var.bastion_security_group_id]))
+
+  // the compact() function checks for null values and gets rid of them 
+  // the length is a check to ensure we dont have an empty array, as an empty array would throw an error for the cidr_block argument 
+  ipv4_cidr_block = length(compact(data.aws_subnet.subnets[*].cidr_block)) == 0 ? null : concat(data.aws_subnet.subnets[*].cidr_block, var.cidrs)
+  ipv6_cidr_block = length(compact(data.aws_subnet.subnets[*].ipv6_cidr_block)) == 0 ? null : concat(data.aws_subnet.subnets[*].ipv6_cidr_block, var.ipv6_cidrs)
 }
 

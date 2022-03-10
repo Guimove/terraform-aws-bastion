fea66a13370edad78f17d730b6bea4e3f88cea3e
alsidneio <alsidneio@gmail.com>
2022-03-09 19:53:49 -0800

    added null validator to the ipv4 block as well

--- locals.tf
+++ locals.tf
@@ -4,6 +4,7 @@ locals {
 
   // the compact() function checks for null values and gets rid of them 
   // the length is a check to ensure we dont have an empty array, as an empty array would throw an error for the cidr_block argument 
+  ipv4_cidr_block = length(compact(data.aws_subnet.subnets[*].cidr_block)) == 0 ? null : concat(data.aws_subnet.subnets[*].cidr_block, var.cidrs)
   ipv6_cidr_block = length(compact(data.aws_subnet.subnets[*].ipv6_cidr_block)) == 0 ? null : concat(data.aws_subnet.subnets[*].ipv6_cidr_block, var.ipv6_cidrs)
 }
 

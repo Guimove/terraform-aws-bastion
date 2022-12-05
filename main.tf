63565b9430ae66c8b6630071754bbacb1dc9dff9
Tony Cosentini <tony@revenuecat.com>
2022-12-05 10:45:29 +0800

    Disable metadata endpoints on bastion instances.

--- main.tf
+++ main.tf
@@ -247,6 +247,10 @@ resource "aws_launch_template" "bastion_launch_template" {
     tags          = merge(tomap({ "Name" = var.bastion_launch_template_name }), merge(var.tags))
   }
 
+  metadata_options {
+    http_endpoint = "disabled"
+  }
+
   lifecycle {
     create_before_destroy = true
   }

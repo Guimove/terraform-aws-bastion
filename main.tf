ac17402ac609b8e4fa65bc6b39daed2dfc2dc927
Peter Mason <peter.mason1@email.com>
2023-01-12 16:55:33 +0000

    pm/add s3 public access block for bastion bucket

--- main.tf
+++ main.tf
@@ -68,6 +68,16 @@ resource "aws_s3_bucket" "bucket" {
   tags = merge(var.tags)
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
 resource "aws_s3_bucket_object" "bucket_public_keys_readme" {
   bucket  = aws_s3_bucket.bucket.id
   key     = "public-keys/README.txt"
